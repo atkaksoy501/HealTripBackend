bc9eded2aede98ed3f608fd49169ab0a2e732d73
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-02-18 16:52:55 +0300

    created Patient, Retreat, User entities. Deleted mock entity class.

---
+++ src/main/java/codewizards/heal_trip/entities/Retreat.java
@@ -0,0 +1,25 @@
+package codewizards.heal_trip.entities;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Data
+@Entity
+@Table(name="retreat")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Retreat {
+    @Id
+    @Column(name = "retreat_id")
+    private int retreat_id;
+
+    @Column(name = "retreat_name")
+    private String retreat_name;
+
+//    @OneToMany(mappedBy = "retreat") //Department class'ı oluşturulduktan sonra açılacak. int olan tip Department olarak değiştirilecek.
+    private int department_id;
+
+    @Column(name = "description")
+    private String description;
+}
