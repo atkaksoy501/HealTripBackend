ad0062405fc7f403049447e555669f5ecd48adb0
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-02-22 21:12:08 +0300

    dto to dao

--- src/main/java/codewizards/heal_trip/dataAccess/UserDao.java
+++ src/main/java/codewizards/heal_trip/dataAccess/UserDao.java
@@ -4,5 +4,5 @@ import codewizards.heal_trip.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserDTO extends JpaRepository<User, Integer>{
+public interface UserDao extends JpaRepository<User, Integer>{
 }
