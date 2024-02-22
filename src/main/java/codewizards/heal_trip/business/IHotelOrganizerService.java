ab42a2956ecd0ea9a35f3ad12cfdfbd6ab635109
Maze Baba <azizalpyolcu09@outlook.com>
2024-02-22 21:34:28 +0300

    hotel-hospitalorg-hotelorg

---
+++ src/main/java/codewizards/heal_trip/business/IHotelOrganizerService.java
@@ -0,0 +1,13 @@
+package codewizards.heal_trip.business;
+
+import codewizards.heal_trip.entities.Hotel;
+import codewizards.heal_trip.entities.HotelOrganizer;
+import java.util.List;
+
+public interface IHotelOrganizerService {
+    List<HotelOrganizer> getAll();
+    List<HotelOrganizer> getAll(int pageNo, int pageSize);
+    void add(HotelOrganizer hotelOrganizer);
+    HotelOrganizer getById(int id);
+    void deleteById(int id);
+}
--- src/main/java/codewizards/heal_trip/dataAccess/aaa.java
+++
@@ -1,4 +0,0 @@
-package codewizards.heal_trip.dataAccess;
-
-public class aaa {
-}
