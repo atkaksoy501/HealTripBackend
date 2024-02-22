97bfddbfee0ced32b6e4ce9fd7ef18628d420ff4
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-02-22 13:38:12 +0300

    registerPatient method shortened

--- src/main/java/codewizards/heal_trip/business/PatientService.java
+++ src/main/java/codewizards/heal_trip/business/PatientService.java
@@ -20,20 +20,10 @@ public class PatientService implements IPatientService{
     }
 
     public Integer registerPatient(Patient patient) {
-        Patient newPatient = new Patient();
-        newPatient.setFirst_name(patient.getFirst_name());
-        newPatient.setLast_name(patient.getLast_name());
-        newPatient.setEmail(patient.getEmail());
-        newPatient.setPhone_number(patient.getPhone_number());
-        newPatient.setUser_password(patient.getUser_password());
-        newPatient.setUser_role("patient");
-        newPatient.setActive(true);
-        newPatient.setBirth_date(patient.getBirth_date());
-        newPatient.setGender(patient.getGender());
-        newPatient.setPatient_height(patient.getPatient_height());
-        newPatient.setPatient_weight(patient.getPatient_weight());
-        newPatient = patientDTO.save(newPatient);
-        return newPatient.getId();
+        patient.setUser_role("patient");
+        patient.setActive(true);
+        patient = patientDTO.save(patient);
+        return patient.getId();
     }
 
     public Patient updatePatient(int patient_id, Patient patient) {
