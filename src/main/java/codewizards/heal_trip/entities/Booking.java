cafe0b472845f05023c5d4e4faf99b46991e5ba5
burakonurerten <burakonurerten@gmail.com>
2024-02-25 13:49:18 +0300

    add relations for Booking and Patient

--- src/main/java/codewizards/heal_trip/entities/Booking.java
+++ src/main/java/codewizards/heal_trip/entities/Booking.java
@@ -1,7 +1,8 @@
 package codewizards.heal_trip.entities;
 
-import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.Date;
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,30 +17,56 @@ public class Booking {
     @Column(name = "id")
     private int id;
     
-    @Column(name = "patient_id")
-    private int patientId;
+    //@Column(name = "patient_id")
+    //private int patientId;
 
-    @Column(name = "hospital_id")
-    private int hospitalId;
+    //@Column(name = "hospital_id")
+    //private int hospitalId;
 
-    @Column(name = "hotel_id")
-    private int hotelId;
-    
-    @Column(name = "doctor_id")
-    private int doctorId;
+    //@Column(name = "hotel_id")
+    //private int hotelId;
+    //
+    //@Column(name = "doctor_id")
+    //private int doctorId;
     
     @Column(name = "start_date")
-    private Date startDate;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate startDate;
 
     @Column(name = "end_date")
-    private Date endDate;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate endDate;
     
     @Column(name = "booking_date")
-    private Date booking_date;
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate booking_date;
     
     @Column(name = "status")
     private String status;
     
-    @Column(name = "retreat_id")
-    private int retreatId;
+    //@Column(name = "retreat_id")
+    //private int retreatId;
+    
+    @ManyToOne()
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "hospital_id", referencedColumnName = "id")
+    private Hospital hospital;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "hotel_id", referencedColumnName = "id")
+    private Hotel hotel;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "doctor_id", referencedColumnName = "id")
+    private Doctor doctor;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "retreat_id", referencedColumnName = "id")
+    private Retreat retreat;
 }
