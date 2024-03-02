ea9b00482555cdfdb002a5216b9f6c9eaffabc2f
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-02 14:28:07 +0300

    hotel and hospital image classes updated

--- src/main/java/codewizards/heal_trip/entities/HospitalImage.java
+++ src/main/java/codewizards/heal_trip/entities/HospitalImage.java
@@ -10,7 +10,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name="hospital_image")
 @Entity
-public class HospitalImage extends Image {
+public class HospitalImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "image")
+    private byte[] image;
+
     @Column(name = "hospital_image_id")
     private int hospital_image_id;
 
