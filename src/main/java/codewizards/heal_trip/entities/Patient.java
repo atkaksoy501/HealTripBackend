1aeccccf326aad1feac2fa9d2fa084e1f74f7104
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-04 22:02:39 +0300

    added JsonIgnore attribute to bookings

--- src/main/java/codewizards/heal_trip/entities/Patient.java
+++ src/main/java/codewizards/heal_trip/entities/Patient.java
@@ -1,6 +1,7 @@
 package codewizards.heal_trip.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import lombok.*;
@@ -27,7 +28,8 @@ public class Patient extends User{
 
     @Column(name = "patient_weight")
     private int patient_weight;
-    
+
+    @JsonIgnore
     @OneToMany(mappedBy = "patient")
     private List<Booking> bookings;
 }
