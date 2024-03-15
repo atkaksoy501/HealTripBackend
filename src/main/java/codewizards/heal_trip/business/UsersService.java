666cc287084c8a7b7eab9885cc3b9c5de91503ec
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-15 23:02:15 +0300

    auth

--- src/main/java/codewizards/heal_trip/business/UsersService.java
+++ src/main/java/codewizards/heal_trip/business/UsersService.java
@@ -1,30 +1,30 @@
-package codewizards.heal_trip.business;
-
-import codewizards.heal_trip.dataAccess.UsersRepository;
-import codewizards.heal_trip.entities.Users;
-import codewizards.heal_trip.entities.UsersRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@RequiredArgsConstructor
-@Service
-public class UsersService implements IUsersService {
-    private final UsersRepository usersRepository;
-
-    public List<Users> GetAllUsers() {
-        return usersRepository.findAll();
-    }
-
-    public Users AddUser(UsersRequest user) {
-        Users newUser = new Users();
-        newUser.setFirst_name(user.getFirst_name());
-        newUser.setLast_name(user.getLast_name());
-        newUser.setEmail(user.getEmail());
-        newUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        newUser.setRoles(user.getRoles());
-        return usersRepository.save(newUser);
-    }
-}
+//package codewizards.heal_trip.business;
+//
+//import codewizards.heal_trip.dataAccess.UsersRepository;
+//import codewizards.heal_trip.entities.Users;
+//import codewizards.heal_trip.entities.UsersRequest;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class UsersService implements IUsersService {
+//    private final UsersRepository usersRepository;
+//
+//    public List<Users> GetAllUsers() {
+//        return usersRepository.findAll();
+//    }
+//
+//    public Users AddUser(UsersRequest user) {
+//        Users newUser = new Users();
+//        newUser.setFirst_name(user.getFirst_name());
+//        newUser.setLast_name(user.getLast_name());
+//        newUser.setEmail(user.getEmail());
+//        newUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+//        newUser.setRoles(user.getRoles());
+//        return usersRepository.save(newUser);
+//    }
+//}
--- src/main/java/codewizards/heal_trip/controllers/UsersController.java
+++ src/main/java/codewizards/heal_trip/controllers/UsersController.java
@@ -1,31 +1,31 @@
-package codewizards.heal_trip.controllers;
-
-import codewizards.heal_trip.business.IUsersService;
-import codewizards.heal_trip.entities.Users;
-import codewizards.heal_trip.entities.UsersRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/users")
-public class UsersController {
-
-    private final IUsersService IUsersService;
-
-    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
-    @GetMapping("")
-    public List<Users> GetUsers() {
-        return IUsersService.GetAllUsers();
-    }
-
-    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
-    @PostMapping("")
-    public Users GetUsers(@RequestBody UsersRequest user) {
-        return IUsersService.AddUser(user);
-    }
-
-}
+//package codewizards.heal_trip.controllers;
+//
+//import codewizards.heal_trip.business.IUsersService;
+//import codewizards.heal_trip.entities.Users;
+//import codewizards.heal_trip.entities.UsersRequest;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/users")
+//public class UsersController {
+//
+//    private final IUsersService IUsersService;
+//
+//    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
+//    @GetMapping("")
+//    public List<Users> GetUsers() {
+//        return IUsersService.GetAllUsers();
+//    }
+//
+//    @PreAuthorize(value = "hasRole('ROLE_ADMIN')")
+//    @PostMapping("")
+//    public Users GetUsers(@RequestBody UsersRequest user) {
+//        return IUsersService.AddUser(user);
+//    }
+//
+//}
