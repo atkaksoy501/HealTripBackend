aff416fd2edd1680df24540fee2f7d2213b31bbf
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-17 11:39:42 +0300

    retreatImage added

--- src/main/java/codewizards/heal_trip/business/IImageService.java
+++ src/main/java/codewizards/heal_trip/business/IImageService.java
@@ -2,21 +2,24 @@ package codewizards.heal_trip.business;
 
 import codewizards.heal_trip.entities.HospitalImage;
 import codewizards.heal_trip.entities.HotelImage;
+import codewizards.heal_trip.entities.RetreatImage;
 
 public interface IImageService {
    HotelImage getHotelImageById(int image_id);
 
     HospitalImage getHospitalImageById(int image_id);
 
+    RetreatImage getRetreatImageById(int image_id);
+
     void saveHotelImage(HotelImage hotelImage);
 
     void saveHospitalImage(HospitalImage hospitalImage);
+
+    void saveRetreatImage(RetreatImage retreatImage);
 
     void deleteHotelImage(int image_id);
 
     void deleteHospitalImage(int image_id);
 
-    //HotelImage getHotelImageByHotelId(int hotel_id);
-
-    //HospitalImage getHospitalImageByHospitalId(int hospital_id);
+    void deleteRetreatImage(int image_id);
 }
--- src/main/java/codewizards/heal_trip/business/ImageService.java
+++ src/main/java/codewizards/heal_trip/business/ImageService.java
@@ -2,9 +2,11 @@ package codewizards.heal_trip.business;
 
 import codewizards.heal_trip.dataAccess.HospitalImageDao;
 import codewizards.heal_trip.dataAccess.HotelImageDao;
+import codewizards.heal_trip.dataAccess.RetreatImageDao;
 import codewizards.heal_trip.entities.HospitalImage;
 import codewizards.heal_trip.entities.Hotel;
 import codewizards.heal_trip.entities.HotelImage;
+import codewizards.heal_trip.entities.RetreatImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,13 @@ import org.springframework.stereotype.Service;
 public class ImageService implements IImageService {
     private HotelImageDao hotelImageDao;
     private HospitalImageDao hospitalImageDao;
+    private RetreatImageDao retreatImageDao;
 
     @Autowired
-    public ImageService(HotelImageDao hotelImageDao, HospitalImageDao hospitalImageDao) {
+    public ImageService(HotelImageDao hotelImageDao, HospitalImageDao hospitalImageDao, RetreatImageDao retreatImageDao) {
         this.hotelImageDao = hotelImageDao;
         this.hospitalImageDao = hospitalImageDao;
+        this.retreatImageDao = retreatImageDao;
     }
     public HotelImage getHotelImageById(int image_id) {
         return hotelImageDao.findById(image_id).orElse(null);
@@ -24,6 +28,10 @@ public class ImageService implements IImageService {
 
     public HospitalImage getHospitalImageById(int image_id) {
         return hospitalImageDao.findById(image_id).orElse(null);
+    }
+
+    public RetreatImage getRetreatImageById(int image_id) {
+        return retreatImageDao.findById(image_id).orElse(null);
     }
 
     public void saveHotelImage(HotelImage hotelImage) {
@@ -34,6 +42,10 @@ public class ImageService implements IImageService {
         hospitalImageDao.save(hospitalImage);
     }
 
+    public void saveRetreatImage(RetreatImage retreatImage) {
+        retreatImageDao.save(retreatImage);
+    }
+
     public void deleteHotelImage(int image_id) {
         hotelImageDao.deleteById(image_id);
     }
@@ -42,11 +54,7 @@ public class ImageService implements IImageService {
         hospitalImageDao.deleteById(image_id);
     }
 
-    //public HotelImage getHotelImageByHotelId(int hotel_id) {
-    //    return hotelImageDao.findByHotelId(hotel_id);
-    //}
-
-    //public HospitalImage getHospitalImageByHospitalId(int hospital_id) {
-    //    return hospitalImageDao.findByHospitalId(hospital_id);
-    //}
+    public void deleteRetreatImage(int image_id) {
+        retreatImageDao.deleteById(image_id);
+    }
 }
