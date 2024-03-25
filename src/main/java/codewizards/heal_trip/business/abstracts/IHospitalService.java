1280f4ba12362af6b0180490e00bcfe822851edc
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-25 14:16:57 +0300

    /hospital/getById dto created

--- src/main/java/codewizards/heal_trip/business/abstracts/IHospitalService.java
+++ src/main/java/codewizards/heal_trip/business/abstracts/IHospitalService.java
@@ -1,12 +1,13 @@
 package codewizards.heal_trip.business.abstracts;
 
+import codewizards.heal_trip.business.DTOs.responses.GotHospitalByIdResponse;
 import codewizards.heal_trip.entities.Doctor;
 import codewizards.heal_trip.entities.Hospital;
 
 import java.util.List;
 
 public interface IHospitalService {
-    Hospital getHospitalById(int hospital_id);
+    GotHospitalByIdResponse getHospitalById(int hospital_id);
 
     Hospital registerHospital(Hospital hospital);
 
