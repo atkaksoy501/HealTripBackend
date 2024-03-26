5a960b854bde1d8d8c3cde8e6115951aa32c8a61
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-03-26 14:22:41 +0300

    address validation enabled

--- src/main/java/codewizards/heal_trip/business/DTOs/requests/UpdateAddressRequest.java
+++ src/main/java/codewizards/heal_trip/business/DTOs/requests/UpdateAddressRequest.java
@@ -1,13 +1,22 @@
 package codewizards.heal_trip.business.DTOs.requests;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UpdateAddressRequest {
+    @NotNull
     private int id;
+    @NotNull
+    @Size(min = 2, max = 15)
     private String country;
+    @NotNull
+    @Size(min = 2, max = 15)
     private String city;
+    @NotNull
+    @Size(min = 2, max = 50)
     private String addressDetail;
 }
