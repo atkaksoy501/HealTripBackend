66b923fed5410fdab978cde0810234227eb006c4
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-20 21:36:32 +0300

    retreat getbyid dtos created

--- src/main/java/codewizards/heal_trip/business/abstracts/IRetreatService.java
+++ src/main/java/codewizards/heal_trip/business/abstracts/IRetreatService.java
@@ -2,6 +2,7 @@ package codewizards.heal_trip.business.abstracts;
 
 import codewizards.heal_trip.business.DTOs.requests.retreat.AddRetreatRequest;
 import codewizards.heal_trip.business.DTOs.responses.retreat.AddedRetreatResponse;
+import codewizards.heal_trip.business.DTOs.responses.retreat.GetRetreatByIdResponse;
 import codewizards.heal_trip.business.DTOs.responses.retreat.GotRetreatByDepartmentIdResponse;
 import codewizards.heal_trip.entities.Retreat;
 
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface IRetreatService {
 
-    Retreat getRetreatById(int retreat_id);
+    GetRetreatByIdResponse getRetreatById(int retreat_id);
 
     AddedRetreatResponse addRetreat(AddRetreatRequest retreat);
 
