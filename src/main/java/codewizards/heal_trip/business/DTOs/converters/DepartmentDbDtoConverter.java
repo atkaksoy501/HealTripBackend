8e3de3df910364387533198705e328091bdf3680
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-21 14:31:20 +0300

    update department dtos done

--- src/main/java/codewizards/heal_trip/business/DTOs/converters/DepartmentDbDtoConverter.java
+++ src/main/java/codewizards/heal_trip/business/DTOs/converters/DepartmentDbDtoConverter.java
@@ -1,9 +1,11 @@
 package codewizards.heal_trip.business.DTOs.converters;
 
 import codewizards.heal_trip.business.DTOs.requests.department.AddDepartmentRequest;
+import codewizards.heal_trip.business.DTOs.requests.department.UpdateDepartmentRequest;
 import codewizards.heal_trip.business.DTOs.responses.doctor.DoctorDTOWithHospital;
 import codewizards.heal_trip.business.DTOs.responses.hospital.GotHospitalByIdResponse;
 import codewizards.heal_trip.business.DTOs.responses.retreat.GetRetreatByIdResponse;
+import codewizards.heal_trip.business.abstracts.IDepartmentService;
 import codewizards.heal_trip.business.abstracts.IDoctorService;
 import codewizards.heal_trip.business.abstracts.IHospitalService;
 import codewizards.heal_trip.business.abstracts.IRetreatService;
@@ -21,7 +23,6 @@ import java.time.LocalDateTime;
 @Service
 public class DepartmentDbDtoConverter{
     private final ModelMapperService modelMapperService;
-    private IDoctorService doctorService;
     private final IHospitalService hospitalService;
     private IRetreatService retreatService;
     @Autowired
@@ -34,12 +35,6 @@ public class DepartmentDbDtoConverter{
 
     @Autowired
     @Lazy
-    public void setDoctorService(IDoctorService doctorService) {
-        this.doctorService = doctorService;
-    }
-
-    @Autowired
-    @Lazy
     public void setRetreatService(IRetreatService retreatService) {
         this.retreatService = retreatService;
     }
@@ -48,6 +43,31 @@ public class DepartmentDbDtoConverter{
     public Department toDbObj(@NotNull AddDepartmentRequest request) {
 
         Department dbDepartment = new Department();
+        dbDepartment.setDepartmentName(request.getDepartmentName());
+        if (request.getHospital_ids() != null) {
+            dbDepartment.setHospitals(request.getHospital_ids().stream().map(hospitalId -> {
+                GotHospitalByIdResponse hospital = hospitalService.getHospitalById(hospitalId);
+                HospitalDepartment hospitalDepartment = new HospitalDepartment();
+                hospitalDepartment.setHospital(modelMapperService.forRequest().map(hospital, Hospital.class));
+                hospitalDepartment.setDepartment(dbDepartment);
+                hospitalDepartment.setCreateDate(LocalDateTime.now());
+                return hospitalDepartment;
+            }).toList());
+        }
+
+        if (request.getRetreat_ids() != null) {
+            dbDepartment.setRetreats(request.getRetreat_ids().stream().map(retreatId -> {
+                GetRetreatByIdResponse retreat = retreatService.getRetreatById(retreatId);
+                return modelMapperService.forRequest().map(retreat, Retreat.class);
+            }).toList());
+        }
+
+        return dbDepartment;
+    }
+
+    @Transactional
+    public Department toDbObj(Department dbDepartment, UpdateDepartmentRequest request, int id) {
+
         dbDepartment.setDepartmentName(request.getDepartmentName());
         if (request.getHospital_ids() != null) {
             dbDepartment.setHospitals(request.getHospital_ids().stream().map(hospitalId -> {
