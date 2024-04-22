5ed242c72c4ea29b7164de991877af607ff41754
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-22 22:43:07 +0300

    doctor dtos done

--- src/main/java/codewizards/heal_trip/business/concretes/DoctorService.java
+++ src/main/java/codewizards/heal_trip/business/concretes/DoctorService.java
@@ -2,13 +2,17 @@ package codewizards.heal_trip.business.concretes;
 
 import codewizards.heal_trip.business.DTOs.converters.DoctorDbDtoConverter;
 import codewizards.heal_trip.business.DTOs.requests.doctor.CreateDoctorRequest;
+import codewizards.heal_trip.business.DTOs.requests.doctor.UpdateDoctorRequest;
 import codewizards.heal_trip.business.DTOs.responses.doctor.DoctorDTOWithHospital;
+import codewizards.heal_trip.business.DTOs.responses.doctor.UpdatedDoctorResponse;
 import codewizards.heal_trip.business.abstracts.IDoctorService;
+import codewizards.heal_trip.core.utilities.mapping.ModelMapperService;
 import codewizards.heal_trip.dataAccess.DoctorDao;
 import codewizards.heal_trip.entities.Doctor;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,6 +20,7 @@ import java.util.List;
 public class DoctorService implements IDoctorService {
     private DoctorDao doctorDao;
     private DoctorDbDtoConverter doctorDbDtoConverter;
+    private ModelMapperService modelMapperService;
 
     @Override
     public DoctorDTOWithHospital getDoctorById(int doctor_id) {
@@ -40,8 +45,11 @@ public class DoctorService implements IDoctorService {
     }
 
     @Override
-    public Doctor updateDoctor(Doctor doctor) {
-        return doctorDao.save(doctor);
+    public UpdatedDoctorResponse updateDoctor(UpdateDoctorRequest doctor, int id) {
+        Doctor dbDoctor = doctorDao.findById(id).orElse(null);
+        Doctor updatedDoctor = doctorDbDtoConverter.toDbObj(dbDoctor, doctor);
+        Doctor savedDoctor = doctorDao.save(updatedDoctor);
+        return doctorDbDtoConverter.toUpdateDto(savedDoctor);
     }
 
     @Override
