91818a9c46c20a6284b19432f16bd2841424e115
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-23 14:12:54 +0300

    roles added

---
+++ src/main/java/codewizards/heal_trip/core/security/Roles.java
@@ -0,0 +1,9 @@
+package codewizards.heal_trip.core.security;
+
+public class Roles {
+    public static final String ADMIN = "ADMIN";
+    public static final String PATIENT = "PATIENT";
+    public static final String HOTEL_ORGANIZER = "HOTEL_ORGANIZER";
+    public static final String HOSPITAL_ORGANIZER = "HOSPITAL_ORGANIZER";
+
+}
