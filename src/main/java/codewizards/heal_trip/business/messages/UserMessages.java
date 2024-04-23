fe6beb391b957317a090ebf1fc3c9116b2030a83
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-23 13:45:06 +0300

    patient business rules

---
+++ src/main/java/codewizards/heal_trip/business/messages/UserMessages.java
@@ -0,0 +1,7 @@
+package codewizards.heal_trip.business.messages;
+
+public class UserMessages {
+
+    public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String USER_NOT_FOUND = "User not found";
+}
