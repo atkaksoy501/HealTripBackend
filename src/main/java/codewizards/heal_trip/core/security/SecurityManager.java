a381b8b350493c796380b1ed0b1ff731de534a61
Atakan Aksoy <atkaksoy501@hotmail.com>
2024-04-23 14:12:54 +0300

    roles added

--- src/main/java/codewizards/heal_trip/core/security/SecurityManager.java
+++ src/main/java/codewizards/heal_trip/core/security/SecurityManager.java
@@ -7,10 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityManager implements SecurityService {
     private static final String[] WHITE_LIST_URLS = {
-            "/swagger-ui/**",
-            "/v2/api-docs/**",
-            "/v3/api-docs/**",
-            "/v3/api-docs/**",
             "/auth/authenticate",
             "/auth/register",
             "/auth/register/patient",
@@ -21,11 +17,54 @@ public class SecurityManager implements SecurityService {
             "/retreat/getByDepartmentId/**"
     };
 
+    private static final String[] SWAGGER_URLS = {
+            "/swagger-ui/**",
+            "/v2/api-docs/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs/**"
+    };
+
+    private static final String[] ADMIN_URLS = {
+            "/retreat/add",
+            "/retreat/update/**",
+            "/retreat/delete/**",
+            "/patient/add",
+            "/patient/delete/**",
+            "/hotelOrganizer/add",
+            "/hotelOrganizer/update/**",
+            "/hotelOrganizer/delete/**",
+            "/hospitalOrganizer/add",
+            "/hospitalOrganizer/update/**",
+            "/hospitalOrganizer/delete/**",
+            "/hotel/add",
+            "/hotel/update/**",
+            "/hotel/delete/**",
+            "/hospital/add",
+            "/hospital/update/**",
+            "/hospital/delete/**",
+            "/doctor/add",
+            "/doctor/update/**",
+            "/doctor/delete/**",
+            "/department/add",
+            "/department/update/**",
+            "/department/delete/**",
+            "/image/retreat/save",
+            "/image/retreat/delete/**",
+            "/image/hotel/save",
+            "/image/hotel/delete/**",
+            "/image/hospital/save",
+            "/image/hospital/delete/**",
+            "/email/send",
+            "/email/sendWelcome",
+            "/email/sendAppointment"
+    };
+
     @Override
     public HttpSecurity configureSecurity(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST_URLS).permitAll()
-//                        .requestMatchers("/swagger-ui/index.html").hasRole("ADMIN")
+                        .requestMatchers(SWAGGER_URLS).hasRole(Roles.ADMIN)
+                        .requestMatchers(ADMIN_URLS).hasRole(Roles.ADMIN)
                         .requestMatchers(HttpMethod.GET).permitAll()
                         .anyRequest().authenticated()
         );
