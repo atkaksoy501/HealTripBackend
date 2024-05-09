eae290ac0e102ef3f1881b482731aa2bac31fcb2
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-05-09 16:44:39 +0300

    hospital is active check added

--- src/main/java/codewizards/heal_trip/business/rules/HospitalBusinessRules.java
+++ src/main/java/codewizards/heal_trip/business/rules/HospitalBusinessRules.java
@@ -3,6 +3,7 @@ package codewizards.heal_trip.business.rules;
 import codewizards.heal_trip.business.messages.HospitalMessages;
 import codewizards.heal_trip.core.utilities.exceptions.types.BusinessException;
 import codewizards.heal_trip.dataAccess.HospitalDao;
+import codewizards.heal_trip.entities.Hospital;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,8 @@ public class HospitalBusinessRules {
     private final HospitalDao hospitalDao;
     private final DepartmentBusinessRules departmentBusinessRules;
     public void checkHospitalExists(int hospitalId) {
-        if (!hospitalDao.existsById(hospitalId)) {
+        Hospital hospital = this.hospitalDao.findById(hospitalId).orElse(null);
+        if (hospital == null || !hospital.isActive()) {
             throw new BusinessException(HospitalMessages.HOSPITAL_NOT_FOUND);
         }
     }
